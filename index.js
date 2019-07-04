d149ada0645a38a4b046dbf8b0ebb8e7af847a79
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-04 17:34:39 +1000

    HTTPS redirect
    for Heroku, thanks to https://jaketrent.com/post/https-redirect-node-heroku/
    
    Also some formatting

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cookieSession = require('cookie-session');
-const passport = require('passport');
-const bodyParser = require('body-parser');
-const keys = require('./config/keys');
-require('./models/User');
-require('./models/Survey');
-require('./services/passport');
+const express = require("express");
+const mongoose = require("mongoose");
+const cookieSession = require("cookie-session");
+const passport = require("passport");
+const bodyParser = require("body-parser");
+const keys = require("./config/keys");
+require("./models/User");
+require("./models/Survey");
+require("./services/passport");
 
 mongoose.Promise = global.Promise;
 mongoose.connect(keys.mongoURI);
@@ -14,6 +14,17 @@ mongoose.connect(keys.mongoURI);
 const app = express();
 
 app.use(bodyParser.json());
+
+//HTTP --> HTTPS redirect for Heroku
+//thanks https://jaketrent.com/post/https-redirect-node-heroku/
+if (process.env.NODE_ENV === "production") {
+  app.use((req, res, next) => {
+    if (req.header("x-forwarded-proto") !== "https")
+      res.redirect(`https://${req.header("host")}${req.url}`);
+    else next();
+  });
+}
+
 app.use(
   cookieSession({
     maxAge: 30 * 24 * 60 * 60 * 1000,
@@ -23,20 +34,20 @@ app.use(
 app.use(passport.initialize());
 app.use(passport.session());
 
-require('./routes/authRoutes')(app);
-require('./routes/billingRoutes')(app);
-require('./routes/surveyRoutes')(app);
+require("./routes/authRoutes")(app);
+require("./routes/billingRoutes")(app);
+require("./routes/surveyRoutes")(app);
 
-if (process.env.NODE_ENV === 'production') {
+if (process.env.NODE_ENV === "production") {
   // Express will serve up production assets
   // like our main.js file, or main.css file!
-  app.use(express.static('client/build'));
+  app.use(express.static("client/build"));
 
   // Express will serve up the index.html file
   // if it doesn't recognize the route
-  const path = require('path');
-  app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  const path = require("path");
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
   });
 }
 
