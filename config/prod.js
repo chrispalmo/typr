4c5d01d73865e1cabb1c9c47a7a37f5544378229
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-04 15:45:53 +1000

    Update prod.js

--- config/prod.js
+++ config/prod.js
@@ -1,11 +1,30 @@
-// prod.js - production keys here!!
+// PRODUCTION KEYS
+// 			>> git remote add heroku https://git.heroku.com/xxxxx.git
+// SET Config Vars:
+// 			>> heroku config:set SOME_API_CREDENTIAL=somevalue
+// REMOVE a config var
+// 			>> heroku config:unset SOME_API_CREDENTIAL
+// VIEW config vars:
+// 			>> heroku config
+
 module.exports = {
-  googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI: process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY,
-  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
-  sendGridKey: process.env.SEND_GRID_KEY,
-  redirectDomain: process.env.REDIRECT_DOMAIN
+	googleClientID: process.env.GOOGLE_CLIENT_ID,
+	googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+	mongoURI: process.env.MONGO_URI,
+	cookieKey: process.env.COOKIE_KEY,
+	stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+	stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+	sendGridKey: process.env.SEND_GRID_KEY,
+	redirectDomain: process.env.REDIRECT_DOMAIN
 };
+
+/*
+	googleClientID: 			UPDATED - UNTESTED
+	googleClientSecret: 	UPDATED - UNTESTED
+	mongoURI:							NOT UPDATED - UNTESTED
+	cookieKey: 						NOT UPDATED - UNTESTED
+	stripePublishableKey: NOT UPDATED - UNTESTED
+	stripeSecretKey: 			NOT UPDATED - UNTESTED
+	sendGridKey: 					NOT UPDATED - UNTESTED
+	redirectDomain: 			NOT UPDATED - UNTESTED
+*/
