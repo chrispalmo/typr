02332489969e1489f43e154cc8a9272d4e2bd233
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-05 12:03:18 +1000

    Create Register.js

---
+++ client/src/components/landing/Register.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Register = () => {
+	return (
+		<div style={{ textAlign: "center" }}>
+			<h5>
+				email-input password-input register-button Have an account?{" "}
+				<Link to="/login">Log in</Link>
+				Logo-bottom-right
+			</h5>
+		</div>
+	);
+};
+
+export default Register;
