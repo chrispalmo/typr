30a371146bb7d673d2e7c9bbf5bb532c466b525b
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-06 17:38:14 +1000

    Update Register.js

--- client/src/components/landing/Register.js
+++ client/src/components/landing/Register.js
@@ -1,43 +1,54 @@
-import React from "react";
+import React, { Component } from "react";
+import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 
-const Register = () => {
-	return (
-		<div
-			style={{
-				justifyContent: "center",
-				display: "flex",
-				flex: 1
-			}}
-		>
-			<div>
-				<button className="btn amber lighten-1">Sign in with Google</button>
-				<div style={{ display: "flex", paddingTop: "10px" }}>
-					<div style={{ flex: 1 }}>
-						<hr />
-					</div>
-					<div
-						style={{
-							display: "flex",
-							paddingRight: "10px",
-							paddingLeft: "10px"
-						}}
-					>
-						or
-					</div>
-					<div style={{ flex: 1 }}>
-						<hr />
-					</div>
-				</div>
-				<div>[email-input]</div>
-				<div>[password-input]</div>
-				<div>[register-button]</div>
+class Register extends Component {
+	render() {
+		return (
+			<div
+				style={{
+					justifyContent: "center",
+					display: "flex",
+					flex: 1
+				}}
+			>
 				<div>
-					Have an account? <Link to="/login">Log in</Link>
+					<button className="btn amber lighten-1">
+						<a href="/auth/google" style={{ color: "black" }}>
+							Sign In With Google
+						</a>
+					</button>
+					<div style={{ display: "flex", paddingTop: "10px" }}>
+						<div style={{ flex: 1 }}>
+							<hr />
+						</div>
+						<div
+							style={{
+								display: "flex",
+								paddingRight: "10px",
+								paddingLeft: "10px"
+							}}
+						>
+							or
+						</div>
+						<div style={{ flex: 1 }}>
+							<hr />
+						</div>
+					</div>
+					<div>[email-input]</div>
+					<div>[password-input]</div>
+					<div>[register-button]</div>
+					<div>
+						Have an account? <Link to="/login">Log in</Link>
+					</div>
 				</div>
 			</div>
-		</div>
-	);
-};
+		);
+	}
+}
 
-export default Register;
+function mapStateToProps({ auth }) {
+	return { auth };
+}
+
+export default connect(mapStateToProps)(Register);
