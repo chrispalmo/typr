5697970edc205fd98195aa7290814f2a78a7fd2f
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-10 14:59:12 +1000

    Update newsAPI.js

--- services/newsAPI.js
+++ services/newsAPI.js
@@ -3,9 +3,10 @@ const key = require("../config/keys").newsApiKey;
 const newsApi = new NewsAPI(key);
 
 //NOTE: You cannot mix the sources parameter with the country or category parameters.
-module.exports = query => {
+module.exports = {
 	// return newsApi.v2.topHeadlines(query);
-	return newsApi.v2.topHeadlines(query);
+	topHeadlines: newsApi.v2.topHeadlines,
+	sources: newsApi.v2.sources
 };
 
 /*
