337bd5bc93a7aa1370c8eac1bec8c729e33a5ad1
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-11 15:48:27 +1000

    redux dev tools installation

--- client/src/index.js
+++ client/src/index.js
@@ -1,21 +1,29 @@
-import 'materialize-css/dist/css/materialize.min.css';
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
-import reduxThunk from 'redux-thunk';
+import "materialize-css/dist/css/materialize.min.css";
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware, compose } from "redux";
+import reduxThunk from "redux-thunk";
 
-import App from './components/App';
-import reducers from './reducers';
+import App from "./components/App";
+import reducers from "./reducers";
 
 // Development only axios helpers!
 // Allows us to send test API requests to the backend (a third-party app such as Postman would be too difficult because we need to include cookies with requests)
-import axios from 'axios';
+import axios from "axios";
 window.axios = axios;
 
-const store = createStore(reducers, {}, applyMiddleware(reduxThunk));
+//redux dev tools helper
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(
+	reducers,
+	composeEnhancers(applyMiddleware(reduxThunk))
+);
 
 ReactDOM.render(
-  <Provider store={store}><App /></Provider>,
-  document.querySelector('#root')
+	<Provider store={store}>
+		<App />
+	</Provider>,
+	document.querySelector("#root")
 );
