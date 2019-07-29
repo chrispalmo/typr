a3ac24bd99b6dc1040af927df46edc7c96e8d39a
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-07-29 13:51:04 +1000

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cookieSession = require("cookie-session");
 const passport = require("passport");
+const newsApi = require("./services/NewsApi");
 const bodyParser = require("body-parser");
 const keys = require("./config/keys");
 require("./models/User");
