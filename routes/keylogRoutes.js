a1ba0d5959d4a5a8f6aa3e84c52eed7f59ddb7d9
chrispalmo <34981948+chrispalmo@users.noreply.github.com>
2019-08-18 12:44:02 +1000

    Update keylogRoutes.js

--- routes/keylogRoutes.js
+++ routes/keylogRoutes.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 const requireLogin = require("../middlewares/requireLogin");
 const User = require("../models/User");
 const KeyEvent = require("../models/KeyEvent");
+const statistics = require("../helpers/statistics.js");
 
 //TODO -- IMPLETEMENT ERROR HANDLING MIDDLEWARE
 
@@ -10,7 +11,7 @@ module.exports = app => {
 	app.post("/api/keylog", async (req, res) => {
 		const first_new_key_event_ts = req.body[0].timestamp;
 		const user = await User.findOne({ _id: req.user._id });
-		//find all keyEvents associated with the current with a timestamp range overlapping that of the uploaded keyEvents
+		//find all keyEvents associated with the current user with a timestamp range overlapping that of the uploaded keyEvents
 		//$gte === greater than or equal to
 		query = {
 			_user: user._id,
@@ -31,4 +32,12 @@ module.exports = app => {
 		});
 	});
 	//
+	app.get("/api/keylog/stats/alltime", async (req, res) => {
+		console.log("Incoming GET request to /api/keylog/stats/alltime");
+		const keyEvents = await KeyEvent.find({ _user: req.user._id }).sort({
+			timestamp: 1
+		});
+		const stats = statistics.analyze(keyEvents, statistics.auto_pause_delay);
+		res.send(stats);
+	});
 };
